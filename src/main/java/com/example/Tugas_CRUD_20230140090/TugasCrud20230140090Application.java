4521d799e49b014bf4c8cd45a87db946db9fcc87
mauraaninditakirana <kiranamaura351@gmail.com>
2026-03-13 23:27:22 +0700

    Initial commit

---
+++ src/main/java/com/example/Tugas_CRUD_20230140090/TugasCrud20230140090Application.java
@@ -0,0 +1,13 @@
+package com.example.Tugas_CRUD_20230140090;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasCrud20230140090Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasCrud20230140090Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tugas_CRUD_20230140090/TugasCrud20230140090ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tugas_CRUD_20230140090;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasCrud20230140090ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
